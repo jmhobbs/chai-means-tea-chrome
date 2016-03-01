2dfc4500d5800ab4dc570c16725a55755d34ea6c
John Hobbs <john@velvetcache.org>
2016-03-01 15:04:44 -0600

    Chai means tea.

---
+++ ChaiTea/chaitea.js
@@ -0,0 +1,23 @@
+function getTextNodesIn(elem, opt_fnFilter) {
+  var textNodes = [];
+  if (elem) {
+    for (var nodes = elem.childNodes, i = nodes.length; i--;) {
+      var node = nodes[i], nodeType = node.nodeType;
+      if (nodeType == 3) {
+        if (!opt_fnFilter || opt_fnFilter(node, elem)) {
+          textNodes.push(node);
+        }
+      }
+      else if (nodeType == 1 || nodeType == 9 || nodeType == 11) {
+        textNodes = textNodes.concat(getTextNodesIn(node, opt_fnFilter));
+      }
+    }
+  }
+  return textNodes;
+}
+
+getTextNodesIn(document.body, function(textNode, parent) {
+  if (/(chai)\s+tea\b/ig.test(textNode.nodeValue)) {
+    textNode.nodeValue = textNode.nodeValue.replace(/(chai)\s+tea\b/ig, '$1');
+  }
+});
---
+++ README.md
@@ -0,0 +1,1 @@
+Chai means tea.
